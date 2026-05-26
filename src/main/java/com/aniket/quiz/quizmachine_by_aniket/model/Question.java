a0d53e7207ac23537f5ca0d88b9f17023d5e5d67
Aniket Desai <aniketdesai@Manalis-MacBook-Air.local>
2026-05-26 10:39:30 +0530

    updated displaying of one question at a time

--- src/main/java/com/aniket/quiz/quizmachine_by_aniket/model/Question.java
+++ src/main/java/com/aniket/quiz/quizmachine_by_aniket/model/Question.java
@@ -1,7 +1,7 @@
 package com.aniket.quiz.quizmachine_by_aniket.model;
 public class Question{
     public String questionText;
-    public String[] options = new String[4];
+    public String[]options = new String[4];
     
     // this will be set using answer parser OR service
     public int correctIndex;
